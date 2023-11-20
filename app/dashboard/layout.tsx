8f396f4c51f608121613f839f2182d9928c8263f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-20 11:48:07 +0100

    Create a layout route for dashboard pages

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SideNav from '../ui/dashboard/sidenav';
+
+const Layout = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className='flex flex-col h-screen md:flex-row md:overflow-hidden'>
+        <div className='w-full flex-none md:w-64'>
+            <SideNav />
+        </div>
+        <div className='flex-grow p-6 md:p-12 md:overflow-y-auto'>{children}</div>
+    </div>
+  )
+}
+
+export default Layout;
