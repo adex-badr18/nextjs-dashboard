35328a3eb84e4cfc7de1c22c233ae530ae177bd5
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2025-09-30 17:56:22 +0100

    feat: Stream dashboard page with route groups

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,22 @@
-const Page = () => {
-  return (
-    <div>Invoices Page</div>
-  )
-}
+import { fetchCardData, fetchLatestInvoices } from "@/app/lib/data";
+import { Card } from "@/app/ui/dashboard/cards";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
 
-export default Page
+const Page = async () => {
+    const latestInvoices = await fetchLatestInvoices();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Invoices
+            </h1>
+
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <LatestInvoices latestInvoices={latestInvoices} />
+            </div>
+        </main>
+    );
+};
+
+export default Page;
