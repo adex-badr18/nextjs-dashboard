cfe8d201272aa44c8b63baf3d45e0b5b3b0a0d61
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2025-10-24 15:38:47 +0100

    feat: manage error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,10 +28,17 @@ export const createInvoice = async (formData: FormData) => {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date) 
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date) 
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    
+  } catch (error) {
+    console.error(error)
+    return { message: "Database Error: Failed to creatre invoice." }
+  }
+
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -46,17 +53,25 @@ export const updateInvoice = async (id: string, formData: FormData) => {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  
+  } catch (error) {
+    console.error(error);
+    return { message: "Database Error: Failed to update invoice." };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 };
 
 export const deleteInvoice = async (id: string) => {
+  // throw new Error("Failed to delete invoice.");
+
   await sql`DELETE FROM invoices WHERE id = ${id}`;
 
   revalidatePath("/dashboard/invoices");
