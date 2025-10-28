d45828b19a608286ec912d1cdfc8550c0ef7494f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2025-10-28 21:29:07 +0100

    feat: Setup authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -112,3 +115,34 @@ export const deleteInvoice = async (id: string) => {
 
   revalidatePath("/dashboard/invoices");
 };
+
+export const authenticate = async (prevState: string | undefined, formData: FormData) => {
+  try {
+    // await signIn("credentials", formData)
+
+    const response = await signIn("credentials", {
+      ...Object.fromEntries(formData),
+      redirect: false,
+      redirectTo: "/dashboard"
+    })
+
+    if (response?.error) {
+      if (response.error === "CredentialsSignin") {
+        return "Invalid credentials.";
+      }
+      return "Something went wrong";
+    }
+
+    redirect("/dashboard");
+
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong";
+      }
+    }
+  }
+}
